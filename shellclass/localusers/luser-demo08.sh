5575b42b822cbe8dfe441a9b940e64e79fa1b21b
dominikhustava <dominikhustava1@gmail.com>
2022-03-15 16:51:27 +0100

    redirecting and explicit std in out err handling

--- shellclass/localusers/luser-demo08.sh
+++ shellclass/localusers/luser-demo08.sh
@@ -7,7 +7,7 @@ FILE="/tmp/data"
 head -n1 /etc/passwd > ${FILE}
 
 # Redirect STDIN to a program.
-read LINE < ${file}
+read LINE < ${FILE}
 echo "LINE contains: ${LINE}"
 
 # Redirect STDOUT to a file, overwriting the file.
@@ -21,4 +21,25 @@ echo "${RANDOM} ${RANDOM}" >> ${FILE}
 echo "${RANDOM} ${RANDOM}" >> ${FILE}
 echo
 echo "Contents of ${FILE}"
+cat ${FILE}
+
+# Redirect STDIN to a program, using FD 0.
+read LINE 0< ${FILE}
+echo
+echo "LINE contains: ${LINE}"
+
+# Redirect STDOUT to a file using FD 1(explicitly), overwriting the file.
+head -n3 /etc/passwd 1> ${FILE}
+echo
+echo "Contents of ${FILE}:"
+cat ${FILE}
+
+# Redirect STDERR to a file usinf FD 2.
+ERR_FILE="/tmp/data.err"
+head -n3 /etc/passwd /nonexistingfile 2> ${ERR_FILE}
+
+# Redirect STDOUT and STDERR to a file.
+head -n3 /etc/passwd /nonexistingfile &> ${FILE}
+echo
+echo "Contents of ${FILE}:"
 cat ${FILE}
